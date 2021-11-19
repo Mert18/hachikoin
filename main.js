08db6368298dc978cd14d5cc00370e621ef1b931
Mert18 <mertuygur02@gmail.com>
2021-11-19 21:28:31 +0300

    basic implementation

---
+++ main.js
@@ -0,0 +1,65 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.timestamp +
+        this.previousHash +
+        JSON.stringify(this.data)
+    ).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2021", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+let hachikoin = new Blockchain();
+hachikoin.addBlock(new Block(1, "10/07/2021", { amount: 4 }));
+hachikoin.addBlock(new Block(2, "2/07/2021", { amount: 3 }));
+hachikoin.addBlock(new Block(3, "1/07/2021", { amount: 12 }));
+
+console.log(hachikoin.getLatestBlock());
+console.log("Is blockchain valid?: ", hachikoin.isChainValid());

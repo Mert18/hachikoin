bd03d1da1b6d8d199a41d626d98c2b674b61a53a
Mert18 <mertuygur02@gmail.com>
2021-11-19 21:37:43 +0300

    difficulty and things

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
@@ -14,14 +15,26 @@ class Block {
       this.index +
         this.timestamp +
         this.previousHash +
+        +this.nonce +
         JSON.stringify(this.data)
     ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block mined: " + this.hash);
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
 
   createGenesisBlock() {
@@ -34,7 +47,7 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -57,9 +70,8 @@ class Blockchain {
 }
 
 let hachikoin = new Blockchain();
-hachikoin.addBlock(new Block(1, "10/07/2021", { amount: 4 }));
-hachikoin.addBlock(new Block(2, "2/07/2021", { amount: 3 }));
-hachikoin.addBlock(new Block(3, "1/07/2021", { amount: 12 }));
 
-console.log(hachikoin.getLatestBlock());
-console.log("Is blockchain valid?: ", hachikoin.isChainValid());
+console.log("Minin block 1...");
+hachikoin.addBlock(new Block(1, "10/07/2021", { amount: 4 }));
+console.log("Minin block 1...");
+hachikoin.addBlock(new Block(2, "2/07/2021", { amount: 3 }));
